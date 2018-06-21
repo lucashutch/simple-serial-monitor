7578f5f8e7aa714de97eed263d46becc0aa4f8e6
lucas Hutchinson <lucas.hutchinson@gmail.com>
2018-06-21 15:33:36 +1200

    first version

---
+++ monitor.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+import serial
+import sys
+import time
+
+
+print("--------------Simple Serial Monitor-------------")
+
+if str(sys.argv[1]) == "-h":
+    print("Useage:  * Arg_1: Port  * Arg_2: Baud           ")
+    print("         * ctrl+c to quit                       ")
+    print("------------------------------------------------")
+    sys.exit(1)
+
+print("This session: Port: " + str(sys.argv[1]) + " Baud: " + str(sys.argv[2]))
+print("------------------------------------------------")
+
+with serial.Serial(sys.argv[1], sys.argv[2], timeout=1) as ser:
+    while True:
+        try:
+            time.sleep(0.1)
+            bytesToRead = ser.inWaiting() # get the amount of bytes available at the input queue
+            if bytesToRead:
+                line = ser.read(bytesToRead)
+                print(line.strip().decode('utf-8'))
+        except serial.SerialException:
+            print("Monitor: Disconnected (Serial exception)")
+            sys.exit(1)
+        except IOError:
+            print("Monitor: Disconnected (I/O Error)")
+            sys.exit(1)
+        except KeyboardInterrupt:
+            print("Monitor: Keyboard Interrupt. Exiting Now...")
+            sys.exit(1)
