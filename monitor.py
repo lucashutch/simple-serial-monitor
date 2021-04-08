3e1d94c42d54f73baff25617904c9f6f9b0a11aa
Lucas Hutchinson <lucas@syrp.co.nz>
2021-04-08 14:50:04 +1200

    update arguments to handle config.txt file
    added config.txt

--- monitor.py
+++ monitor.py
@@ -7,51 +7,56 @@ import os
 from datetime import datetime
 import string
 
-baud = 2000000
-
-if(len(sys.argv) > 3 or len(sys.argv) < 2):
-    print("Incorrect arguments. run with -h to see options")
-    sys.exit(1)
-
-print("--------------Simple Serial Monitor-------------")
-if str(sys.argv[1]) == "-h":
-    print("Useage:")
-    print("    monitor.py <port> <baud_rate>\n")
-    print("    port        serial port of system to listen")
-    print("                eg COM3 on windows, USB0 on linux (omit `/dev/tty` prefix on linux)")
-    print("    baud_rate   baud rate of serial link")
-    print("                Baudrate defaults to 2M Baud if not specified")
-    print("    ctrl+c      quit\n")
-    print("    Example 1: Create a serial monitor on /dev/ttyUSB0 @ 115200 baud")
-    print("        'monitor.py USB0 115200'")
-    print("    Example 2: Create a serial monitor on COM3 on windows @ 2000000 Baud")
-    print("        'monitor.py COM3'")
-    sys.exit(1)
-
-if(len(sys.argv) == 3):
-    baud = sys.argv[2]
+# clear screen
 os.system('cls' if os.name == 'nt' else 'clear')
 
-print("This session: Port: /dev/tty" + str(sys.argv[1]) + " Baud: " + str(baud))
+baud = 2000000
+port = "ACM0"
+
+# make sure config file is loaded from script dir rather than cwd
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+config_file_full_path = os.path.join(__location__,"config.txt")
+
+if(len(sys.argv) >= 2):
+    port = str(sys.argv[1])
+    if(len(sys.argv) == 3):
+        baud = sys.argv[2]
+    else:
+        print("Baud not specified, using default")
+else:
+    try:
+        with open(config_file_full_path, "r") as config_file:
+            port = config_file.readline().strip()
+            baud = config_file.readline().strip()
+    except:
+        print("No config file or args. Using default")
+        pass
+
+print("This session: Port: /dev/tty" + port + " Baud: " + str(baud))
 print("------------------------------------------------")
 
 serial_prefix = "/dev/tty"
 if os.name == 'nt':
     serial_prefix = ""
 
-with serial.Serial(serial_prefix + sys.argv[1], baud, timeout=1) as ser:
-    ser.flush()
-    while True:
-        try:
-            line = ser.readline()
-            decoded_line = line.decode('utf-8', errors="ignore")
-            print(decoded_line, end="")
-        except serial.SerialException:
-            print("Monitor: Disconnected (Serial exception)")
-            sys.exit(1)
-        except IOError:
-            print("Monitor: Disconnected (I/O Error)")
-            sys.exit(1)
-        except KeyboardInterrupt:
-            print("Monitor: Keyboard Interrupt. Exiting Now...")
-            sys.exit(1)
+def main():
+    with serial.Serial(serial_prefix + port, baud, timeout=1) as ser:
+        ser.flush()
+        while True:
+            try:
+                line = ser.readline()
+                decoded_line = line.decode('utf-8', errors="ignore")
+                print(decoded_line, end='')
+            except serial.SerialException:
+                print("Monitor: Disconnected (Serial exception)")
+                sys.exit(1)
+            except IOError:
+                print("Monitor: Disconnected (I/O Error)")
+                sys.exit(1)
+            except KeyboardInterrupt:
+                print("Monitor: Keyboard Interrupt. Exiting Now...")
+                sys.exit(1)
+
+if (__name__ == '__main__'): 
+    main()
